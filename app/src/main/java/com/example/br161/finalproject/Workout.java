d8566282258e3dd48296452ba77ca2d3c6f6450a
Thurind <d-maddux@hotmail.com>
2015-05-04 13:14:37 -0500

    Got the inital setup for the recycler view done

---
+++ app/src/main/java/com/example/br161/finalproject/Workout.java
@@ -0,0 +1,24 @@
+package com.example.br161.finalproject;
+
+/**
+ * Created by My Laptop on 5/4/2015.
+ */
+public class Workout {
+
+    private String title;
+
+    public Workout(String t)
+    {
+        title = t;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/br161/finalproject/WorkoutAdapter.java
@@ -0,0 +1,60 @@
+package com.example.br161.finalproject;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+
+public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHolder> {
+
+    List<Workout> workouts;
+
+    public WorkoutAdapter(List<Workout> workouts) { this.workouts = workouts; }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate((R.layout.workout_item, parent, false);
+
+        final ViewHolder viewHolder = new ViewHolder(view, new ViewHolder.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() { return workouts.size(); }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ItemClickListener listener;
+
+
+        public ViewHolder(View itemView, ItemClickListener listener) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onItemClick(view, getAdapterPosition());
+        }
+
+        public interface ItemClickListener {
+            void onItemClick(View view, int position);
+        }
+    }
+}
+
